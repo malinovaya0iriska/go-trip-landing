8895d16e19809c7bf6e73405a0a712c38c873e40
Irina <malinovayairiska@gmail.com>
2022-08-28 13:42:47 +0300

    feat: add tiny slider

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ navBtn.addEventListener("click", () => {
   menuIcon.classList.toggle("menu-icon--active");
   document.body.classList.toggle("no-scroll");
 });
+
+// Tiny Slider
+const prevBtn = document.querySelector(".slider__btn--prev");
+const nextBtn = document.querySelector(".slider__btn--next");
+
+const slider = tns({
+  container: ".carousel",
+  startIndex: 0,
+  items: 1,
+  gutter: 20,
+  slideBy: "page",
+  autoplay: true,
+  prevButton: prevBtn,
+  nextButton: nextBtn,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  lazyload: true,
+  lazyloadSelector: ".slider__item",
+  center: true,
+  touch: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  responsive: {
+    540: {
+      items: 3,
+      gutter: 10,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
